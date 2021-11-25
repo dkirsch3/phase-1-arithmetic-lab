3c71b72acd9a548d0427fcb67be7e2019420d307
David <trey3@hotmail.com>
2021-11-24 19:47:33 -0600

    commit tree 01373d8c3fdf5c71bd5b94053b4765873faaf3dd

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let num2 = 31
 
 let multiply = num1*num2
 
-let random = Math.ceil(Math.random()*100)+1
+let random = Math.floor(Math.random()*100)+1
 
 let num3 = 16
 let num4 = 4
@@ -13,3 +13,4 @@ let num4 = 4
 let mod = num3/num4
 
 let max = Math.max(1,2,3,4,9,1,20)
+
